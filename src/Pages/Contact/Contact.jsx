9bbf9c97018c6bfc558f7d507ec8591e61fb05ce
krishnamoorthy3 <krishnamoorthym3009@gmail.com>
2025-01-17 19:08:00 +0530

    updated contact file

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,9 +1,18 @@
 
 const Contact = () => {
     return (
-        <div>
-
-        </div>
+        <>
+            <section>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-6">
+                            
+                        </div>
+                        <div className="col-md-6"></div>
+                    </div>
+                </div>
+            </section>
+        </>
     )
 }
 
