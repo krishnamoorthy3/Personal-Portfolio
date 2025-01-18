f7946dbde5f18c5450d84cf2798cbf5d6c7cc38a
krishnamoorthy3 <krishnamoorthym3009@gmail.com>
2025-01-18 18:28:05 +0530

    About file and image updated

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,8 +1,57 @@
-
+import Aboutimg from "../../assets/images/about2.png"
+import { GoDotFill } from "react-icons/go";
+import Reacticon from "../../assets/images/react.svg";
+import "./About.css"
 const About = () => {
     return (
         <>
+            <section className="about-sect-1-wrap">
+                <div className="container">
+                    <div className="row align-items-center">
+                        <div className="col-md-5">
+                            <img src={Aboutimg} alt="" className="w-100" />
+                        </div>
+                        <div className="col-md-7 about-pad-right">
+                            <h2 className="home-title-h">About Me</h2>
+                            <h2 className="home-title">Mark Hendry</h2>
+                            <p className="abt-d-role">Software Developer</p>
+                            <p className="abt-p-content">A Product Designer & Developer and I am in the game for over 7+ years. I am proud of my works and ready to face the next challenge</p>
+                            <p className="abt-p-content">That is where I come in. A lover of words, a wrangler of copy. Here to create copy that not only reflects who you are and what you stand for, but words that truly land with those that read them, calling your audience in and making them .</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section>
 
+            </section>
+            <section className="abt-sec-2-wrap">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-6 col-12">
+                            <h2 className="home-title-h">Experience</h2>
+                            <div className="exp-abt-wrap">
+                                <p className="exp-abt-date">03/216 – Running</p>
+                                <h5 className="exp-abt-title"><GoDotFill/>Axtra</h5>
+                                <h4 className="exp-abt-de">Lead digital marketer</h4>
+                            </div>
+                        </div>
+                        <div className="col-md-6 col-12">
+                            <h2 className="home-title-h">Education</h2>
+                            <div className="exp-abt-wrap">
+                                <p className="exp-abt-date">03/216 – Running</p>
+                                <h5 className="exp-abt-title"><GoDotFill/>Axtra</h5>
+                                <h4 className="exp-abt-de">Lead digital marketer</h4>
+                            </div>
+                        </div>
+                    </div>
+                    <h2 className="home-title-h">Skills</h2>
+                    <div className="d-flex flex-wrap gap-4 my-5">
+                        <div className="exp-abt-wrap m-0">
+                            <h4 className="exp-abt-de"><img src={Reacticon} alt="" /></h4>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </>
     )
 }
--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,14 +1,53 @@
-
+import { IoMail } from "react-icons/io5";
+import { MdCall } from "react-icons/md";
+import { FaLocationDot } from "react-icons/fa6";
+import { FaArrowRight } from "react-icons/fa";
+import "./Contact.css"
+import { Link } from "react-router-dom";
 const Contact = () => {
     return (
         <>
-            <section>
+            <section className="cont-sec-wrapper">
                 <div className="container">
                     <div className="row">
-                        <div className="col-md-6">
-                            
+                        <div className="col-md-6 cont-pad-right">
+                            <h2 className="home-title-h">Contact</h2>
+                            <h3 className="home-title">Let’s <br />
+                            connect</h3>
+
+                            <Link className="contact-a-d"><IoMail/><span>hello@henry.com</span></Link>
+                            <Link className="contact-a-d"><MdCall/><span>+1 234 567 890</span></Link>
+                            <Link className="contact-a-d"><FaLocationDot/><span>Bengaluru</span></Link>
                         </div>
-                        <div className="col-md-6"></div>
+                        <div className="col-md-6">
+                            <form action="">
+                                <div className="row row-gap-4">
+                                    <div className="col-md-6">
+                                        <label htmlFor="" className="form-label">Name</label>
+                                        <input type="text" className="form-input" placeholder="Your Name" />
+                                    </div>
+                                    <div className="col-md-6">
+                                        <label htmlFor=""  className="form-label">Email</label>
+                                        <input type="email" className="form-input" placeholder="Your Email"  />
+                                    </div>
+                                    <div className="col-md-6">
+                                        <label htmlFor=""  className="form-label">Phone</label>
+                                        <input type="tel" className="form-input" placeholder="Your Phone Number"/>
+                                    </div>
+                                    <div className="col-md-6">
+                                        <label htmlFor=""  className="form-label">Subject</label>
+                                        <input type="text"  className="form-input" placeholder="Your Subject"/>
+                                    </div>
+                                    <div className="col-12">
+                                        <label htmlFor=""  className="form-label">Message</label>
+                                        <textarea name="" id="" rows="4" className="form-textarea"  placeholder="Your Message"/>
+                                    </div>
+                                    <div className="col-12">
+                                        <button className="contact-sub-btn">Submite <FaArrowRight/></button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </section>
